d0d630bff26ddd20d942cbd2dd9491660f509e5a
Mike Witts <mike@witts.xyz>
2021-11-26 11:46:00 +0000

    added simple looping script for lab

---
+++ clear-ADSyncStatus.ps1
@@ -0,0 +1,7 @@
+# Re-run command to disable AD Sync after removal of AAD 
+# DO NOT USE ON PRODUCTION SYSTEMS
+
+while ((Get-MSOLCompanyInformation).DirectorySynchronizationEnabled) {
+    Set-MsolDirSyncEnabled -EnableDirSync $False -Force
+    Start-Sleep -Seconds 3600
+}
